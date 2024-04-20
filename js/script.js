9e2789efb3a287fda60f953be07223448d934747
vini-j-pereira <viniciusjosepereira@gmail.com>
2024-04-19 23:24:06 -0300

    commit_inicial

---
+++ js/script.js
@@ -0,0 +1,16 @@
+class Produto {
+
+    constructor() {
+
+    }
+
+    adicionar() {
+        alert('Produto add!')
+    }
+
+    excluir() {
+        alert('Item excluido!')
+    }
+}
+
+var produto = new Produto()
